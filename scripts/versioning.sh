08cff8a85c8786e855d7376425b5b464438f8f02
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2025-02-24 15:39:47 +0530

    Update versioning.sh

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Fetch the current version from the version.txt file
-CURRENT_VERSION=$(cat version.txt)
+# Versioning logic to increment versions
+VERSION_FILE="version.txt"
+if [ -f "$VERSION_FILE" ]; then
+    CURRENT_VERSION=$(cat $VERSION_FILE)
+    echo "Current version: $CURRENT_VERSION"
+else
+    CURRENT_VERSION="1.0.0"
+    echo "No version file found. Using default version: $CURRENT_VERSION"
+fi
 
-# Split the current version into its components (major.minor.patch)
 IFS='.' read -r -a VERSION_PARTS <<< "$CURRENT_VERSION"
 MAJOR=${VERSION_PARTS[0]}
 MINOR=${VERSION_PARTS[1]}
 PATCH=${VERSION_PARTS[2]}
 
-# Increment the patch version (you can modify this logic based on your versioning strategy)
+# Increment patch version by 1
 PATCH=$((PATCH + 1))
 
-# Construct the new version
 NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
 
-# Save the new version back to version.txt
-echo "$NEW_VERSION" > version.txt
-
-# Output the new version
-echo "New Version: $NEW_VERSION"
+# Save the new version back to the file
+echo $NEW_VERSION > $VERSION_FILE
+echo "New version: $NEW_VERSION"
