083bde45dbb81f8cc4b5c25127c4822b5e8e6a8d
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2025-02-27 14:36:34 +0530

    Update utilities.sh

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Utility function to clean up old build logs
 cleanup_logs() {
     find ./build_log/build_logs -type f -mtime +30 -exec rm {} \;
     echo "Cleaned up old build logs."
 }
 
-# Call cleanup
 cleanup_logs
