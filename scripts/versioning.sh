eb40353c64b77e0c5a2aa27d77d4946df675dfad
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2025-02-27 14:36:52 +0530

    Update versioning.sh

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Versioning logic to increment versions
 VERSION_FILE="version.txt"
 if [ -f "$VERSION_FILE" ]; then
     CURRENT_VERSION=$(cat $VERSION_FILE)
@@ -15,11 +14,9 @@ MAJOR=${VERSION_PARTS[0]}
 MINOR=${VERSION_PARTS[1]}
 PATCH=${VERSION_PARTS[2]}
 
-# Increment patch version by 1
 PATCH=$((PATCH + 1))
 
 NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}"
 
-# Save the new version back to the file
 echo $NEW_VERSION > $VERSION_FILE
 echo "New version: $NEW_VERSION"
